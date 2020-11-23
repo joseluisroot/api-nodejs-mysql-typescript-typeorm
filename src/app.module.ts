79ccf9b49e9ae53f7259860502871e0a913e3bac
José Luis Reyes <joseluis.r85@gmail.com>
2020-11-23 07:34:03 -0600

    add update in configurations files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { SkillsController } from './skills/skills.controller';
+import { SkillsService } from './skills/skills.service';
+import { SkillsModule } from './skills/skills.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  imports: [
+    // UsersModule,
+    // SkillsModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: '',
+    //   database: 'dbCV',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+  ],
+  controllers: [AppController, UsersController, SkillsController],
+  providers: [AppService, UsersService, SkillsService],
 })
 export class AppModule {}
